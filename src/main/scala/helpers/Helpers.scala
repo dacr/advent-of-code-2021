821089e4bc322e87fed20bd9c98d0659665a5e7e
Crosson David <crosson.david@gmail.com>
2021-12-01 18:08:45 +0100

    Improve file IO helper functions

--- src/main/scala/helpers/Helpers.scala
+++ src/main/scala/helpers/Helpers.scala
@@ -6,26 +6,31 @@ import java.nio.charset.Charset
 
 object Helpers {
 
-  def readResourceContent(resourceName: String) = {
-    for {
+  def readResourceContent(resourceName: String, charsetName: String = "UTF-8") = {
+    for { // KO to enhance
       inputStream <- IO.attempt(Helpers.getClass.getResourceAsStream(resourceName))
       bytes       <- IO.attemptBlockingIO(inputStream.readAllBytes())
-      content     <- IO.attempt(new String(bytes, "UTF-8"))
+      content     <- IO.attempt(new String(bytes, charsetName))
     } yield content
   }
 
-  def readPathContent(inputPath: Path) = for {
-    charset <- IO.attempt(Charset.forName("UTF-8"))
+  def readPathContent(inputPath: Path, charsetName: String = "UTF-8") = for {
+    charset <- IO.attempt(Charset.forName(charsetName))
     content <- IO.attemptBlockingIO(Files.readString(inputPath, charset))
   } yield content
 
-  def readFileContent(filename: String) = for {
+  def readFileContent(filename: String, charsetName: String = "UTF-8") = for {
     inputPath <- IO.attempt(Path.of(filename))
-    content   <- readPathContent(inputPath)
+    content   <- readPathContent(inputPath, charsetName)
   } yield content
 
-  def writePathContent(outputPath: Path, content: String) = for {
-    charset <- IO.attempt(Charset.forName("UTF-8"))
+  def writePathContent(outputPath: Path, content: String, charsetName: String = "UTF-8") = for {
+    charset <- IO.attempt(Charset.forName(charsetName))
     _       <- IO.attemptBlockingIO(Files.writeString(outputPath, content, charset))
+  } yield ()
+
+  def writeFileContent(filename: String, content: String, charsetName: String = "UTF-8") = for {
+    outputPath <- IO.attempt(Path.of(filename))
+    _          <- writePathContent(outputPath, content, charsetName)
   } yield ()
 }
