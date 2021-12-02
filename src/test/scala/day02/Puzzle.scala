3e6c996338d499bc540f0998a05493755eed20e1
Crosson David <crosson.david@gmail.com>
2021-12-02 06:37:47 +0100

    Solve day2

--- src/test/scala/day02/Puzzle.scala
+++ src/test/scala/day02/Puzzle.scala
@@ -8,12 +8,33 @@ import zio.test.Assertion.*
 import zio.test.TestAspect.{failing, ignore}
 
 object Puzzle {
-  def resolveStar1(input:String):Int = {
-    ???
+  // ------------------------------------------------------------------------------
+  def line2tuple(line: String) =
+    line match {
+      case s"forward $f" => (f.toInt, 0)
+      case s"down $d"    => (0, d.toInt)
+      case s"up $d"      => (0, -d.toInt)
+    }
+
+  def resolveStar1(input: String): Int = {
+    input
+      .split("\n")
+      .map(line2tuple)
+      .reduce { case ((f1, d1), (f2, d2)) => (f1 + f2, d1 + d2) } match { case (f, d) => f * d }
   }
 
-  def resolveStar2(input:String):Int = {
-    ???
+  // ------------------------------------------------------------------------------
+
+  case class Position(horizontal: Int = 0, depth: Int = 0, aim: Int = 0)
+  def resolveStar2(input: String): Int = {
+    val finalPosition =
+      input
+        .split("\n")
+        .map(line2tuple)
+        .foldLeft(Position()) { case (pos, (h, d)) =>
+          Position(pos.horizontal + h, pos.depth + pos.aim * h, pos.aim + d)
+        }
+    finalPosition.depth * finalPosition.horizontal
   }
 }
 
@@ -27,7 +48,7 @@ object Puzzle02Test extends DefaultRunnableSpec {
         exampleResult = Puzzle.resolveStar1(exampleInput)
         puzzleInput  <- Helpers.readFileContent(s"data/$day-puzzle-given-1.txt")
         puzzleResult  = Puzzle.resolveStar1(puzzleInput)
-      } yield assertTrue(exampleResult == 0) && assertTrue(puzzleResult == 0)
+      } yield assertTrue(exampleResult == 150) && assertTrue(puzzleResult == 1693300)
     },
     test("star#2") {
       for {
@@ -35,7 +56,7 @@ object Puzzle02Test extends DefaultRunnableSpec {
         exampleResult = Puzzle.resolveStar2(exampleInput)
         puzzleInput  <- Helpers.readFileContent(s"data/$day-puzzle-given-1.txt")
         puzzleResult  = Puzzle.resolveStar2(puzzleInput)
-      } yield assertTrue(exampleResult == 0) && assertTrue(puzzleResult == 0)
+      } yield assertTrue(exampleResult == 900) && assertTrue(puzzleResult == 1857958050)
     }
   )
 }
