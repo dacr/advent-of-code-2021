91e67c84f6c0c19307d26913498164e2a3dfe527
Crosson David <crosson.david@gmail.com>
2021-12-04 06:37:53 +0100

    star1 achieved

--- src/test/scala/day04/Puzzle.scala
+++ src/test/scala/day04/Puzzle.scala
@@ -9,9 +9,56 @@ import zio.test.TestAspect.{ignore, failing}
 import scala.annotation.tailrec
 
 // ------------------------------------------------------------------------------
+case class ChosenNumbers(numbers: Iterable[Int])
+
+case class Cell(number: Int, marked: Boolean = false)
+
+case class Grid(rows: Iterable[Seq[Cell]]) {
+  def play(number: Int): Grid =
+    Grid(rows.map(row => row.map(cell => cell.copy(marked = if (number == cell.number) true else cell.marked))))
+  def isWin: Boolean          =
+    rows.exists(_.forall(_.marked)) ||
+      0.until(rows.head.size).exists(pos => rows.forall(row => row.lift(pos).filter(_.marked).isDefined))
+}
+
+def parseGrids(lines: List[String]): List[Grid] =
+  lines
+    .filterNot(_.isBlank)
+    .sliding(5, 5)
+    .map(gridLines =>
+      Grid(
+        gridLines
+          .map(
+            _.trim
+              .split("\\s+")
+              .toSeq
+              .map(value => Cell(value.toInt))
+          )
+      )
+    )
+    .toList
+
+def parsePuzzle(input: String): (ChosenNumbers, List[Grid]) =
+  input.split("\n").toList match {
+    case firstLine :: remaining =>
+      (ChosenNumbers(firstLine.trim.split(",").map(_.toInt).toList), parseGrids(remaining))
+  }
+
+// ------------------------------------------------------------------------------
+
+def playGame(numbers: Iterable[Int], grids: List[Grid]): Int =
+  numbers match {
+    case number :: remainingNumbers =>
+      val updatedGrids = grids.map(_.play(number))
+      updatedGrids.find(_.isWin) match {
+        case Some(wonGrid) => number * wonGrid.rows.flatMap(_.filterNot(_.marked).map(_.number)).sum
+        case None          => playGame(remainingNumbers, updatedGrids)
+      }
+  }
 
 def resolveStar1(input: String): Int =
-  0
+  val (chosenNumbers, grids) = parsePuzzle(input)
+  playGame(chosenNumbers.numbers, grids)
 
 // ------------------------------------------------------------------------------
 
@@ -29,7 +76,7 @@ object Puzzle04Test extends DefaultRunnableSpec {
         exampleResult = resolveStar1(exampleInput)
         puzzleInput  <- Helpers.readFileContent(s"data/$day-puzzle-1.txt")
         puzzleResult  = resolveStar1(puzzleInput)
-      } yield assertTrue(exampleResult == -1) && assertTrue(puzzleResult == -1)
+      } yield assertTrue(exampleResult == 4512) && assertTrue(puzzleResult == -1)
     },
     test("star#2") {
       for {
