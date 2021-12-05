6d9630db20f587403699f4c5b43beded3946f618
Crosson David <crosson.david@gmail.com>
2021-12-05 07:08:10 +0100

    Implement day5

--- src/test/scala/day05/Puzzle.scala
+++ src/test/scala/day05/Puzzle.scala
@@ -7,15 +7,50 @@ import zio.test.Assertion.*
 import zio.test.TestAspect.{ignore, failing}
 
 import scala.annotation.tailrec
+import scala.math.*
+
+// ------------------------------------------------------------------------------
+case class Coord(x: Int, y: Int)
+
+case class Vent(start: Coord, end: Coord):
+  def points(): Seq[Coord] = // naive based draw algorithm
+    val dx = end.x - start.x
+    val dy = end.y - start.y
+    if dx == 0 then start.y.to(end.y, signum(dy)).map(y => Coord(start.x, y))
+    else if dy == 0 then start.x.to(end.x, signum(dx)).map(x => Coord(x, start.y))
+    else if abs(dx) > abs(dy) then start.x.to(end.x, signum(dx)).map(x => Coord(x, start.y + dy * (x - start.x) / dx))
+    else start.y.to(end.y, signum(dy)).map(y => Coord(start.x + dx * (y - start.y) / dy, y))
+
+def line2vent(line: String): Vent =
+  line.trim match
+    case s"$x1,$y1 -> $x2,$y2" => Vent(Coord(x1.toInt, y1.toInt), Coord(x2.toInt, y2.toInt))
+
+def parse(input: String): List[Vent] =
+  input
+    .split("\n")
+    .toList
+    .map(line2vent)
 
 // ------------------------------------------------------------------------------
 def resolveStar1(input: String): Int =
-  0
+  val vents = parse(input)
+  vents
+    .filter(vent => vent.start.x == vent.end.x || vent.start.y == vent.end.y)
+    .flatMap(_.points())
+    .groupBy(identity)
+    .view
+    .count((coord, sz) => sz.size >= 2)
 
 // ------------------------------------------------------------------------------
 
 def resolveStar2(input: String): Int =
-  0
+  val vents = parse(input)
+  vents
+    .flatMap(_.points())
+    .groupBy(identity)
+    .view
+    .count((coord, sz) => sz.size >= 2)
+
 
 // ------------------------------------------------------------------------------
 
@@ -28,7 +63,7 @@ object Puzzle05Test extends DefaultRunnableSpec {
         exampleResult = resolveStar1(exampleInput)
         puzzleInput  <- Helpers.readFileContent(s"data/$day-puzzle-1.txt")
         puzzleResult  = resolveStar1(puzzleInput)
-      } yield assertTrue(exampleResult == -1) && assertTrue(puzzleResult == -1)
+      } yield assertTrue(exampleResult == 5) && assertTrue(puzzleResult == -1)
     },
     test("star#2") {
       for {
@@ -36,7 +71,7 @@ object Puzzle05Test extends DefaultRunnableSpec {
         exampleResult = resolveStar2(exampleInput)
         puzzleInput  <- Helpers.readFileContent(s"data/$day-puzzle-1.txt")
         puzzleResult  = resolveStar2(puzzleInput)
-      } yield assertTrue(exampleResult == -1) && assertTrue(puzzleResult == -1)
+      } yield assertTrue(exampleResult == 12) && assertTrue(puzzleResult == -1)
     }
   )
 }
