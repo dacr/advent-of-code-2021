b6c04fa8a1167c188a355a4cd47f328d006787db
Crosson David <crosson.david@gmail.com>
2021-12-07 20:21:12 +0100

    Prepare day08

---
+++ src/test/scala/day08/Puzzle.scala
@@ -0,0 +1,43 @@
+package day08
+
+import helpers.*
+import zio.*
+import zio.test.*
+import zio.test.Assertion.*
+import zio.test.TestAspect.{failing, ignore, timeout}
+
+import scala.annotation.tailrec
+import scala.math.*
+
+// ------------------------------------------------------------------------------
+
+def resolveStar1(input: String): Int =
+  0
+
+// ------------------------------------------------------------------------------
+
+def resolveStar2(input: String): Int =
+  0
+// ------------------------------------------------------------------------------
+
+object Puzzle08Test extends DefaultRunnableSpec {
+  val day  = "day08"
+  def spec = suite(s"puzzle $day")(
+    test("star#1") {
+      for {
+        exampleInput <- Helpers.readFileContent(s"data/$day-example-1.txt")
+        exampleResult = resolveStar1(exampleInput)
+        puzzleInput  <- Helpers.readFileContent(s"data/$day-puzzle-1.txt")
+        puzzleResult  = resolveStar1(puzzleInput)
+      } yield assertTrue(exampleResult == -1) && assertTrue(puzzleResult == -1)
+    },
+    test("star#2") {
+      for {
+        exampleInput <- Helpers.readFileContent(s"data/$day-example-1.txt")
+        exampleResult = resolveStar2(exampleInput)
+        puzzleInput  <- Helpers.readFileContent(s"data/$day-puzzle-1.txt")
+        puzzleResult  = resolveStar2(puzzleInput)
+      } yield assertTrue(exampleResult == -1) && assertTrue(puzzleResult == -1)
+    }
+  )
+}
