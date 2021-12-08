a50e0e49f8f0a25444641ed0bb4ec551bb4c8e75
Crosson David <crosson.david@gmail.com>
2021-12-08 06:46:12 +0100

    Solve day7 star1

--- src/test/scala/day08/Puzzle.scala
+++ src/test/scala/day08/Puzzle.scala
@@ -10,9 +10,50 @@ import scala.annotation.tailrec
 import scala.math.*
 
 // ------------------------------------------------------------------------------
+type Display = Set[Char]
+val segmentsByNumber: Map[Int, Display] = Map(
+  0 -> "abcefg",
+  1 -> "cf",
+  2 -> "acdeg",
+  3 -> "acdfg",
+  4 -> "bcdf",
+  5 -> "abdfg",
+  6 -> "abdefg",
+  7 -> "acf",
+  8 -> "abcdefg",
+  9 -> "abcdfg"
+).map((k, v) => k -> v.toSet)
+
+val uniks                        = Set(1, 4, 7, 8)
+val uniksSegments: Set[Display]  = uniks.flatMap(segmentsByNumber.get)
+val uniksSegmentsSizes: Set[Int] = uniksSegments.map(_.size)
+
+case class Entry(signals: Set[Display], output: List[Display])
+
+def parseEntry(input: String): Entry =
+  input.split(" [|] ", 2) match {
+    case Array(left, right) =>
+      Entry(
+        signals = left.trim.split(" ").map(_.toSet).toSet,
+        output = right.trim.split(" ").map(_.toSet).toList
+      )
+  }
+
+def parseEntries(input: String): Seq[Entry] =
+  input.trim
+    .split("\n")
+    .toSeq
+    .map(parseEntry)
+
+// ------------------------------------------------------------------------------
 
 def resolveStar1(input: String): Int =
-  0
+  val entries = parseEntries(input)
+  entries.map { entry =>
+    entry.output.count { signal =>
+        uniksSegmentsSizes.contains(signal.size)
+    }
+  }.sum
 
 // ------------------------------------------------------------------------------
 
@@ -25,19 +66,20 @@ object Puzzle08Test extends DefaultRunnableSpec {
   def spec = suite(s"puzzle $day")(
     test("star#1") {
       for {
-        exampleInput <- Helpers.readFileContent(s"data/$day-example-1.txt")
+        _            <- ZIO.logInfo(uniksSegmentsSizes.mkString(","))
+        exampleInput <- Helpers.readFileContent(s"data/$day-example-2.txt")
         exampleResult = resolveStar1(exampleInput)
         puzzleInput  <- Helpers.readFileContent(s"data/$day-puzzle-1.txt")
         puzzleResult  = resolveStar1(puzzleInput)
-      } yield assertTrue(exampleResult == -1) && assertTrue(puzzleResult == -1)
+      } yield assertTrue(exampleResult == 26) && assertTrue(puzzleResult == 330)
     },
     test("star#2") {
       for {
-        exampleInput <- Helpers.readFileContent(s"data/$day-example-1.txt")
+        exampleInput <- Helpers.readFileContent(s"data/$day-example-2.txt")
         exampleResult = resolveStar2(exampleInput)
         puzzleInput  <- Helpers.readFileContent(s"data/$day-puzzle-1.txt")
         puzzleResult  = resolveStar2(puzzleInput)
       } yield assertTrue(exampleResult == -1) && assertTrue(puzzleResult == -1)
-    }
+    } @@ ignore
   )
 }
