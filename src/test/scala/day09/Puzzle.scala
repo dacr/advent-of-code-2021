00103f1487c4eececdd3ae491f3229dca6a1181a
David Crosson <david.crosson@orange.com>
2021-12-09 19:24:21 +0100

    Refactor

--- src/test/scala/day09/Puzzle.scala
+++ src/test/scala/day09/Puzzle.scala
@@ -19,98 +19,86 @@ def parse(input: String): Cave =
     .toSeq
     .map(line => line.toSeq.map(_.toInt - 48))
 
-// ------------------------------------------------------------------------------
-def isLowest(cave: Cave, coord: Coord): Boolean =
-  val (x, y) = coord
-  isLowest(cave, x, y)
-
-def isLowest(cave: Cave, x: Int, y: Int): Boolean =
-  val reference        = cave(y)(x)
-  val levels: Seq[Int] = (
-    Seq.empty :++
-      cave.lift(y - 1).flatMap(_.lift(x)) :++
-      cave.lift(y + 1).flatMap(_.lift(x)) :++
-      cave.lift(y).flatMap(_.lift(x - 1)) :++
-      cave.lift(y).flatMap(_.lift(x + 1))
-  )
-  levels.forall(_ > reference)
-
-def resolveStar1(input: String): Int =
-  val cave   = parse(input)
+def allCoords(cave: Cave): Iterable[Coord] =
   val width  = cave.head.size
   val height = cave.size
-  val coords = 0.until(width).flatMap(x => 0.until(height).map(y => x -> y))
-  coords
-    .collect { case (x, y) if isLowest(cave, x, y) => cave(y)(x) }
-    .map(_ + 1)
-    .sum
-
-// ------------------------------------------------------------------------------
-
-def lowCoords(cave: Cave): Iterable[Coord] =
-  val width  = cave.head.size
-  val height = cave.size
-  val coords = 0.until(width).flatMap(x => 0.until(height).map(y => x -> y))
-  coords.collect { case (x, y) if isLowest(cave, x, y) => (x, y) }
+  0.until(width).flatMap(x => 0.until(height).map(y => x -> y))
 
 def levelAt(cave: Cave, coord: Coord): Int =
   val (x, y) = coord
   cave(y)(x)
 
-def aroundsOf(cave: Cave, coord: Coord): Iterable[Coord] =
+def coordsAroundOf(cave: Cave, coord: Coord): Iterable[Coord] =
   val (x, y) = coord
-  Seq.empty :++
-    cave.lift(y - 1).flatMap(_.lift(x)).map(_ => x -> (y - 1)) :++
-    cave.lift(y + 1).flatMap(_.lift(x)).map(_ => x -> (y + 1)) :++
-    cave.lift(y).flatMap(_.lift(x - 1)).map(_ => (x - 1) -> y) :++
+  Seq(
+    cave.lift(y - 1).flatMap(_.lift(x)).map(_ => x -> (y - 1)),
+    cave.lift(y + 1).flatMap(_.lift(x)).map(_ => x -> (y + 1)),
+    cave.lift(y).flatMap(_.lift(x - 1)).map(_ => (x - 1) -> y),
     cave.lift(y).flatMap(_.lift(x + 1)).map(_ => (x + 1) -> y)
+  ).flatten
 
-def isUnvisitedLowest(cave: Cave, coord: Coord, visited: Seq[Coord]): Boolean =
-  val (x, y)    = coord
-  val reference = cave(y)(x)
-  val levels    =
-    aroundsOf(cave, coord)
-      .filterNot(visited.contains)
-      .collect { case (x, y) => cave(y)(x) }
-  levels.forall(_ > reference)
-
-def display(cave: Cave, selected: Set[Coord]): Unit =
+def render(cave: Cave, selected: Set[Coord]): String =
   val width  = cave.head.size
   val height = cave.size
-  val repr =
-    0.until(height).map { y =>
-      0.until(width).map { x =>
-        if selected.contains(x -> y) then levelAt(cave, x -> y).toString else "."
-      }.mkString
-    }.mkString("\n")
-  println(repr)
+  0.until(height)
+    .map { y =>
+      0.until(width)
+        .map { x =>
+          if selected.contains(x -> y) then levelAt(cave, x -> y).toString else "."
+        }
+        .mkString
+    }
+    .mkString("\n")
+
+// ------------------------------------------------------------------------------
+def isLowest(cave: Cave, coord: Coord): Boolean =
+  val (x, y)           = coord
+  val reference        = levelAt(cave, coord)
+  val levels = coordsAroundOf(cave, coord).map(coord => levelAt(cave, coord))
+  levels.forall(_ > reference)
+
+def lowestCoords(cave:Cave):Iterable[Coord] =
+  allCoords(cave)
+    .filter(coord => isLowest(cave, coord))
+
+
+def resolveStar1(input: String): Int =
+  val cave = parse(input)
+  lowestCoords(cave)
+    .map(coord => levelAt(cave, coord))
+    .map(_ + 1)
+    .sum
+
+// ------------------------------------------------------------------------------
+
+def selectable(cave: Cave, coord: Coord, visited: Seq[Coord], limit:Int): Boolean =
+  val reference = levelAt(cave, coord)
+  val levels    =
+    coordsAroundOf(cave, coord)
+      .filterNot(visited.contains)
+      .map(coord => levelAt(cave, coord))
+  levels.forall(_ > reference) && levelAt(cave, coord) < limit
 
 def basinArea(cave: Cave, from: Coord, limit: Int = 9): List[Coord] =
   @tailrec
   def walk(toVisit: List[Coord], visited: Seq[Coord], accu: List[Coord]): List[Coord] =
-    //display(cave, accu.toSet)
-    toVisit match {
+    // display(cave, accu.toSet)
+    toVisit match
       case Nil => accu
-
-      case head :: remaining if levelAt(cave, head) >= limit =>
-        walk(remaining, visited :+ head, accu)
-
-      case head :: remaining if isUnvisitedLowest(cave, head, visited) =>
-        val arounds = aroundsOf(cave, head).filterNot(remaining.contains).filterNot(accu.contains)
+      case head :: remaining if selectable(cave, head, visited, limit)  =>
+        val arounds = coordsAroundOf(cave, head).filterNot(remaining.contains).filterNot(accu.contains)
         walk(remaining :++ arounds, visited :+ head, head :: accu)
-
       case head :: remaining =>
         walk(remaining, visited :+ head, accu)
-    }
+
   val area = walk(List(from), Nil, Nil)
-  //display(cave, area.toSet)
+  // display(cave, area.toSet)
   area
 
-def resolveStar2(input: String): Long =
+def resolveStar2(input: String): Int =
   val cave = parse(input)
-  val lows = lowCoords(cave)
-  lows
-    .map(coord => basinArea(cave, coord).size.toLong)
+  lowestCoords(cave)
+    .map(coord => basinArea(cave, coord).size)
     .toList
     .sortBy(-_)
     .take(3)
@@ -135,10 +123,10 @@ object Puzzle09Test extends DefaultRunnableSpec {
         exampleResult1 = resolveStar2(exampleInput1)
         puzzleInput   <- Helpers.readFileContent(s"data/$day-puzzle-1.txt")
         puzzleResult   = resolveStar2(puzzleInput)
-      } yield assertTrue(exampleResult1 == 1134L) &&
-        assertTrue(puzzleResult > 1017600L) &&
-        assertTrue(puzzleResult < 280596750L) &&
-        assertTrue(puzzleResult == 1263735L)
+      } yield assertTrue(exampleResult1 == 1134) &&
+        assertTrue(puzzleResult > 1017600) &&
+        assertTrue(puzzleResult < 280596750) &&
+        assertTrue(puzzleResult == 1263735)
     }
   )
 }
