d105c9574c7911205d747f8034620d20fec0d50d
Crosson David <crosson.david@gmail.com>
2021-12-09 06:20:26 +0100

    Solve Day#8 Star#1

--- src/test/scala/day09/Puzzle.scala
+++ src/test/scala/day09/Puzzle.scala
@@ -8,11 +8,38 @@ import zio.test.TestAspect.{failing, ignore, timeout}
 
 import scala.annotation.tailrec
 import scala.math.*
+import scala.util.chaining.*
+
+type Cave = Seq[Seq[Int]]
+
+def parse(input: String): Cave =
+  input
+    .split("\n")
+    .toSeq
+    .map(line => line.toSeq.map(_.toInt-48))
 
 // ------------------------------------------------------------------------------
+def isLowest(cave: Cave, x: Int, y: Int): Boolean =
+  val reference        = cave(y)(x)
+  val levels: Seq[Int] =(
+    Seq.empty :++
+      cave.lift(y - 1).flatMap(_.lift(x)) :++
+      cave.lift(y + 1).flatMap(_.lift(x)) :++
+      cave.lift(y).flatMap(_.lift(x - 1)) :++
+      cave.lift(y).flatMap(_.lift(x + 1))
+    )
+    levels.forall(_ > reference)
 
 def resolveStar1(input: String): Int =
-  0
+  val cave   = parse(input)
+  val width  = cave.head.size
+  val height = cave.size
+  val coords = 0.until(width).flatMap(x => 0.until(height).map(y => x -> y))
+  coords
+    .collect { case (x, y) if isLowest(cave, x, y) => cave(y)(x) }
+    .tap(println)
+    .map(_ + 1)
+    .sum
 
 // ------------------------------------------------------------------------------
 
@@ -30,7 +57,7 @@ object Puzzle09Test extends DefaultRunnableSpec {
         exampleResult = resolveStar1(exampleInput)
         puzzleInput  <- Helpers.readFileContent(s"data/$day-puzzle-1.txt")
         puzzleResult  = resolveStar1(puzzleInput)
-      } yield assertTrue(exampleResult == -1) && assertTrue(puzzleResult == -1)
+      } yield assertTrue(exampleResult == 15) && assertTrue(puzzleResult == 452)
     },
     test("star#2") {
       for {
