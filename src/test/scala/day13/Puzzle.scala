cb0e85a79daedc4a4c89b311422b4f8548ff1936
Crosson David <crosson.david@gmail.com>
2021-12-13 19:24:19 +0100

    Solve Day#13 stars

--- src/test/scala/day13/Puzzle.scala
+++ src/test/scala/day13/Puzzle.scala
@@ -11,19 +11,66 @@ import scala.math.*
 import scala.util.chaining.*
 
 // ------------------------------------------------------------------------------
-def parse(input:String)=
-  input
-    .split("\n")
+case class Dot(x: Int, y: Int)
+case class FoldInstruction(horizontal: Boolean, position: Int)
+
+def parseDot(input: String): Dot =
+  input.split(",", 2) match
+    case Array(xs, ys) => Dot(xs.toInt, ys.toInt)
+
+def parseFoldInstruction(input: String): FoldInstruction =
+  input match
+    case s"fold along $a=$p" if a == "x" => FoldInstruction(false, p.toInt)
+    case s"fold along $a=$p" if a == "y" => FoldInstruction(true, p.toInt)
+
+def parse(input: String): (Set[Dot], List[FoldInstruction]) =
+  val Array(coordsSection, instructionsSection) = input.split("\n\n", 2).map(_.trim)
+  val coords                                    = coordsSection.split("\n").map(parseDot)
+  val instructions                              = instructionsSection.split("\n").map(parseFoldInstruction)
+  (coords.toSet, instructions.toList)
+
+// ------------------------------------------------------------------------------
+
+def paperSize(dots:Set[Dot]):(Int,Int) =
+  val maxx=dots.maxBy(dot=>dot.x).x
+  val maxy=dots.maxBy(dot=>dot.y).y
+  (maxx, maxy)
+
+def render(dots:Set[Dot]):String =
+  val (maxx, maxy) = paperSize(dots)
+  0.to(maxx).map(x=> 0.to(maxy).map(y=> if dots.contains(Dot(x,y)) then "#" else " ").mkString).mkString("\n")
+
+def foldPaper(dots: Set[Dot], instructions: List[FoldInstruction]): Set[Dot] =
+  //println("---------------------")
+  //println(render(dots))
+  instructions match
+    case Nil                                                              => dots
+    case FoldInstruction(horizontal, position) :: remaining if horizontal =>
+      val up     = dots.filter(_.y < position)
+      val down   = dots.filter(_.y > position)
+      val folded = down.map(dot => Dot(dot.x, 2 * position - dot.y))
+      foldPaper(up ++ folded, remaining)
+    case FoldInstruction(horizontal, position) :: remaining               =>
+      val left   = dots.filter(_.x < position)
+      val right  = dots.filter(_.x > position)
+      val folded = right.map(dot => Dot(2 * position - dot.x, dot.y))
+      foldPaper(left ++ folded, remaining)
 
 // ------------------------------------------------------------------------------
 
 def resolveStar1(input: String): BigInt =
-  0
+  val (dots, instrs) = parse(input)
+  val folded = foldPaper(dots, instrs.head::Nil)
+  folded.size
 
 // ------------------------------------------------------------------------------
 
 def resolveStar2(input: String): BigInt =
-  0
+  val (dots, instrs) = parse(input)
+  val folded = foldPaper(dots, instrs)
+  //println(render(folded)) // =>DISPLAY : HKUJGAJZ
+  folded.size
+
 
 // ------------------------------------------------------------------------------
 
@@ -36,7 +83,7 @@ object Puzzle13Test extends DefaultRunnableSpec {
         exampleResult = resolveStar1(exampleInput)
         puzzleInput  <- Helpers.readFileContent(s"data/$day-puzzle-1.txt")
         puzzleResult  = resolveStar1(puzzleInput)
-      } yield assertTrue(exampleResult == BigInt(0)) && assertTrue(puzzleResult == BigInt(0))
+      } yield assertTrue(exampleResult == BigInt(17)) && assertTrue(puzzleResult == BigInt(621))
     },
     test("star#2") {
       for {
@@ -44,7 +91,7 @@ object Puzzle13Test extends DefaultRunnableSpec {
         exampleResult1 = resolveStar2(exampleInput1)
         puzzleInput   <- Helpers.readFileContent(s"data/$day-puzzle-1.txt")
         puzzleResult   = resolveStar2(puzzleInput)
-      } yield assertTrue(exampleResult1 == BigInt(0)) &&assertTrue(puzzleResult == BigInt(0))
+      } yield assertTrue(exampleResult1 == BigInt(16)) && assertTrue(puzzleResult == BigInt(95))
     }
   )
 }
