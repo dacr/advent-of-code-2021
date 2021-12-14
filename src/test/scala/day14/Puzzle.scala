2d73059ab3caeefe513b832e38248221f7b67725
Crosson David <crosson.david@gmail.com>
2021-12-14 09:01:42 +0100

    Solve Day#14 Star#2

--- src/test/scala/day14/Puzzle.scala
+++ src/test/scala/day14/Puzzle.scala
@@ -40,7 +40,7 @@ def reduceTemplate(template: Template, rules: Rules): Template =
         .sliding(2, 1)
         .flatMap(pair => applyRule(rules, pair).tail)
         .toSeq
-  //println(template.mkString+"->"+result.mkString)
+  // println(template.mkString+"->"+result.mkString)
   result
 
 def reduceFlow(template: Template, rules: Rules): LazyList[Template] =
@@ -56,11 +56,58 @@ def resolveStar1(input: String): BigInt =
   mostCommonFreq - leastCommonFreq
 
 // ------------------------------------------------------------------------------
+// REBOOT
 
+case class Pair2(left: Char, right: Char)
+case class Count2(count: BigInt, leftCount: BigInt, rightCount: BigInt)
+type Rules2      = Map[Pair2, Char]
 
+def reduceTemplate2(template: Map[Pair2, Count2], rules: Rules2): Map[Pair2, Count2] =
+  template.toList
+    .flatMap { (pair, count) =>
+      rules.get(pair) match {
+        case None     => (pair -> count) :: Nil
+        case Some(ch) =>
+          val leftPair  = Pair2(pair.left, ch)
+          val rightPair = Pair2(ch, pair.right)
+          (leftPair    -> Count2(count.count, count.leftCount, count.count)) ::
+            (rightPair -> Count2(count.count, 0, count.rightCount)) :: Nil
+      }
+    }
+    .groupMapReduce((k, v) => k)((k, v) => v) { (a, b) =>
+      Count2(
+        a.count + b.count,
+        a.leftCount + b.leftCount,
+        a.rightCount + b.rightCount
+      )
+    }
 
-def resolveStar2(input: String): BigInt =
-  0
+def reduceFlow2(template: Map[Pair2, Count2], rules: Rules2): LazyList[Map[Pair2, Count2]] =
+  template #:: reduceFlow2(reduceTemplate2(template, rules), rules)
+
+def resolveStar2(input: String, upTo: Int): BigInt =
+  val (template, rules) = parse(input)
+  val rules2            = rules.collect { case (Seq(l, r), v) => Pair2(l, r) -> v }
+  val tuples            =
+    template
+      .sliding(2, 1)
+      .collect { case Seq(l, r) => Pair2(l, r) -> Count2(1, 0, 1) }
+      .toList
+  val fixedTuples       =
+    (tuples.head match { case (k, v) => k -> Count2(1, 1, 1) }) :: tuples.tail
+
+  val flow  = reduceFlow2(fixedTuples.toMap, rules2)
+  val found = flow.drop(upTo).head
+
+  val freqs =
+    found.toList
+      .flatMap { case (pair, count) => (pair.left -> count.leftCount) :: (pair.right -> count.rightCount) :: Nil }
+      .groupMapReduce((k, v) => k)((k, v) => v)(_ + _)
+
+  val (mostCommonElement, mostCommonFreq)   = freqs.maxBy { case (c, count) => count }
+  val (leastCommonElement, leastCommonFreq) = freqs.minBy { case (c, count) => count }
+
+  mostCommonFreq - leastCommonFreq
 
 // ------------------------------------------------------------------------------
 
@@ -78,10 +125,13 @@ object Puzzle14Test extends DefaultRunnableSpec {
     test("star#2") {
       for {
         exampleInput1 <- Helpers.readFileContent(s"data/$day-example-1.txt")
-        exampleResult1 = resolveStar2(exampleInput1)
-        puzzleInput   <- Helpers.readFileContent(s"data/$day-puzzle-1.txt")
-        puzzleResult   = resolveStar2(puzzleInput)
-      } yield assertTrue(exampleResult1 == BigInt("2188189693529")) && assertTrue(puzzleResult == BigInt(0))
+        exampleResult1 = resolveStar2(exampleInput1, 10)
+         exampleResult2 = resolveStar2(exampleInput1,40)
+         puzzleInput   <- Helpers.readFileContent(s"data/$day-puzzle-1.txt")
+         puzzleResult   = resolveStar2(puzzleInput, 40)
+      } yield assertTrue(exampleResult1 == BigInt(1588))
+       && assertTrue(exampleResult2 == BigInt("2188189693529"))
+       && assertTrue(puzzleResult == BigInt("3542388214529"))
     }
   )
 }
